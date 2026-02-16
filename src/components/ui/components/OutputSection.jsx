17833fc5b67c705515b37cd43a83df49b09bb8a3
kongtup Tangsatjatham <kongtupjai@gmail.com>
2026-02-16 19:28:34 +0700

    Building OutputSection

---
+++ src/components/ui/components/OutputSection.jsx
@@ -0,0 +1,7 @@
+export default function OutputSection(){
+    return (
+        <div className="bg-white h-100 w-80 dark:text-black flex items-center justify-center">
+            Pokemon picture
+        </div>
+    )
+}
