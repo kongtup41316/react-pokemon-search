644dbb117acef6ea7f0cd5fa67a4fca318fe860a
kongtup Tangsatjatham <kongtupjai@gmail.com>
2026-02-18 15:30:23 +0700

    Fetching Apis

--- src/components/ui/components/OutputSection.jsx
+++ src/components/ui/components/OutputSection.jsx
@@ -3,10 +3,24 @@ import axios from "axios";
 
 export default function OutputSection(){
 // https://pokeapi.co/api/v2/pokemon/pikachu
+    const [pokemonData, setPokemonData] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async (pokemonName) => {
+            try{
+                const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+                setPokemonData(response.data);
+            }
+            catch(err){
+                console.error("Error fetching data:", err);
+            }
+        }
+        fetchData();
+    }, []);
 
     return (
         <div className="bg-white h-100 w-80 dark:text-black flex items-center justify-center rounded-md">
-            Pokemon picture
+            Pokemon Picture
         </div>
     )
 }
