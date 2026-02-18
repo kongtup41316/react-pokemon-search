51507f4e59400a369c4aa080cab738661e630eb3
kongtup Tangsatjatham <kongtupjai@gmail.com>
2026-02-18 16:12:56 +0700

    Fixing fetchData function

--- src/components/ui/components/OutputSection.jsx
+++ src/components/ui/components/OutputSection.jsx
@@ -1,26 +1,27 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-export default function OutputSection(){
+export default function OutputSection({pokemonName}){
 // https://pokeapi.co/api/v2/pokemon/pikachu
     const [pokemonData, setPokemonData] = useState(null);
 
     useEffect(() => {
-        const fetchData = async (pokemonName) => {
+        const fetchData = async () => {
             try{
-                const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
-                setPokemonData(response.data);
+                const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/pikachu`);
+                setPokemonData(response.data.name);
             }
             catch(err){
                 console.error("Error fetching data:", err);
             }
         }
         fetchData();
-    }, []);
+    }, [pokemonName]);
 
     return (
         <div className="bg-white h-100 w-80 dark:text-black flex items-center justify-center rounded-md">
             Pokemon Picture
+            {console.log(pokemonData)}
         </div>
     )
 }
--- src/components/ui/components/SearchSection.jsx
+++ src/components/ui/components/SearchSection.jsx
@@ -3,14 +3,23 @@ import { Button } from "@/components/ui/ui/button"
 import OutputSection from "./OutputSection"
 
 export default function SearchSection() {
+    const [userInput, setUserInput] = useState("");
+
     return (
         <div className="flex items-center justify-center flex-col gap-10">
             <div className="bg-blue-400  dark:bg-gray-600 w-110 h-40 flex justify-center items-center rounded-md gap-2">
-                <Input placeholder="Search Pokemon.. (ex.Pikachu)" className="bg-white dark:bg-white dark:text-black w-75 dark:placeholder:text-gray-500" />
-                <Button className="cursor-pointer bg-orange-400 dark:bg-orange-400 dark:text-white hover:bg-orange-500 dark:hover:bg-orange-500 transition-bg duration-200">search</Button>
+                <Input 
+                    value={userInput}
+                    placeholder="Search Pokemon.. (ex.Pikachu)" 
+                    className="bg-white dark:bg-white dark:text-black w-75 dark:placeholder:text-gray-500"
+                />
+                <Button 
+                    className="cursor-pointer bg-orange-400 dark:bg-orange-400 dark:text-white hover:bg-orange-500 dark:hover:bg-orange-500 transition-bg duration-200">
+                    search
+                </Button>
             </div>
 
-            <OutputSection />
+            <OutputSection pokemonName = {userInput} />
         </div>
     )
 }
